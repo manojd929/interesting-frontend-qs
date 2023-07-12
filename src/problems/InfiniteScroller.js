fe8c8634895139bafcba9ee02f591cd587a2c6d2
manojthetechguy <manojthetechguy@gmail.com>
2023-07-12 22:07:03 +0530

    infinite scrolling

--- src/problems/InfiniteScroller.js
+++ src/problems/InfiniteScroller.js
@@ -1,42 +1,87 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 const useBookSearch = (query, pageNumber) => {
-  const [] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
   useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    setLoading(true);
+    setError(false);
     fetch(
       'https://openlibrary.org/search.json?' +
-        new URLSearchParams({ q: query, page: pageNumber })
+        new URLSearchParams({ q: query, page: pageNumber }),
+      { signal }
     )
+      .then((res) => res.json())
       .then((res) => {
-        console.log(res.data);
+        setBooks((prevBooks) => [
+          ...prevBooks,
+          ...res.docs?.map((b) => b.title),
+        ]);
+        setHasMore(res.docs.length > 0);
       })
       .catch((err) => {
+        setError(err.toString());
         console.log(err.toString());
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [query, pageNumber]);
+
+  return { loading, error, books, hasMore };
 };
 
 const InfiniteScroller = () => {
   const [query, setQuery] = useState('');
   const [pageNumber, setPageNumber] = useState(1);
 
-  useBookSearch(query, pageNumber);
-
   const handleSearch = (e) => {
     setQuery(e.target.value);
     setPageNumber(1);
   };
 
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef(null);
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+          console.log('Visible');
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
   return (
-    <div>
+    <div style={{ margin: '1rem' }}>
       <section>
         <input type="text" onChange={handleSearch} value={query} />
-        <div>Title</div>
-        <div>Title</div>
-        <div>Title</div>
-        <div>Title</div>
-        <div>Loading...</div>
-        <div>Error</div>
+        <ul>
+          {books.map((book, index) => {
+            if (books.length === index + 1) {
+              return (
+                <li key={index} ref={lastBookElementRef}>
+                  {book}
+                </li>
+              );
+            }
+            return <li key={index}>{book}</li>;
+          })}
+        </ul>
+        {loading && <div>Loading...</div>}
+        {error && <div>{error}</div>}
       </section>
     </div>
   );
